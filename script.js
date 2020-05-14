6daa6437f2ef0e63fbb3b64bcf4d39e27c7307b5
Tom Savage <tsavage@gmail.com>
2020-05-14 09:58:26 -0400

    fixed class issue

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 //form section
+
 var searches = JSON.parse(localStorage.getItem("searches")) || [];
 
 function renderHistory() {
   $("#history").html("");
 
-  searches.forEach(function(e) {
-    $("#history").prepend($("<p>").text(e).addClass("city"));
-  })
+  searches.forEach(function (e) {
+    console.log(e, "<=== this is e");
+    var pTag =$("<p>").text(e).addClass("city")
+    console.log(pTag)
+    $("#history").prepend(pTag);
+  });
+  query(searches[searches.length - 1]);
 }
 
 $("form").on("submit", function (event) {
   event.preventDefault();
   var city = $("#city").val().trim();
-  query(city);
+  // query(city);
   searches.push(city);
-  
 
   localStorage.setItem("searches", JSON.stringify(searches));
   $("#city").val("");
@@ -25,9 +29,6 @@ $("form").on("submit", function (event) {
 $(document).on("click", ".city", function () {
   query($(this).text());
 });
-renderHistory();
-
-
 
 //api data section
 
@@ -35,43 +36,57 @@ function query(city) {
   $("#fiveDayDisplay").empty();
   $("#fiveDayTitle").empty();
   $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c9c478a157f268c22eedc9b26117af86",
+    url:
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=c9c478a157f268c22eedc9b26117af86",
     method: "GET",
   }).then(function (response) {
     //console.log(response);
 
-    $(".city").text(response.name);
+    $(".theCity").text(response.name);
     $(".temp").text("Temperature (F) : " + Math.floor(response.main.temp));
     $(".humidity").text("Humidity: " + response.main.humidity);
     $(".wind").text("Wind Speed: " + response.wind.speed);
   });
-  //using ajax to access api and use information for the 5 day forecast
-   $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=c9c478a157f268c22eedc9b26117af86",
+ // using ajax to access api and use information for the 5 day forecast
+  $.ajax({
+    url:
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&units=imperial&appid=c9c478a157f268c22eedc9b26117af86",
     method: "GET",
-    }).then(function (response) {
+  }).then(function (response) {
     console.log(response);
-    var fiveDay = [response.list[4], response.list[12], response.list[20], response.list[28], response.list[36]]
-    var fiveText = $("<h2>").text("5 day Forecast:")
-    $("#fiveDayTitle").append(fiveText)
-    fiveDay.forEach(function(fiveArray){
-      var fiveDiv = $("<div>").addClass("eachday")
+    var fiveDay = [
+      response.list[4],
+      response.list[12],
+      response.list[20],
+      response.list[28],
+      response.list[36],
+    ];
+    var fiveText = $("<h2>").text("5 day Forecast:");
+    $("#fiveDayTitle").append(fiveText);
+    fiveDay.forEach(function (fiveArray) {
+      var fiveDiv = $("<div>").addClass("eachday");
       var fiveDate = $("<p>").text(fiveArray.dt_txt.split(" ")[0]);
-      var fiveIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + fiveArray.weather[0].icon + "@2x.png")
-      var fiveTemp = $("<p>").text("Temperature: " +Math.floor(fiveArray.main.temp))
-      var fiveHumid = $("<p>").text("Humidity: " + fiveArray.main.humidity +"%")
-      fiveDiv.append( fiveDate, fiveIcon, fiveTemp, fiveHumid);
+      var fiveIcon = $("<img>").attr(
+        "src",
+        "https://openweathermap.org/img/wn/" +
+          fiveArray.weather[0].icon +
+          "@2x.png"
+      );
+      var fiveTemp = $("<p>").text(
+        "Temperature: " + Math.floor(fiveArray.main.temp)
+      );
+      var fiveHumid = $("<p>").text(
+        "Humidity: " + fiveArray.main.humidity + "%"
+      );
+      fiveDiv.append(fiveDate, fiveIcon, fiveTemp, fiveHumid);
 
-      $("#fiveDayDisplay").append(fiveDiv)
-  
-  
-     })
-
-  });
-
-
-      
-
+      $("#fiveDayDisplay").append(fiveDiv);
+    });
+ });
 }
 
-
+renderHistory();
