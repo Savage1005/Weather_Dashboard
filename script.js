2c780969458c1f229a3c7123bc08676c8dbd4017
Tom Savage <tsavage@gmail.com>
2020-05-13 22:01:13 -0400

    update

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ function renderHistory() {
 
 $("form").on("submit", function (event) {
   event.preventDefault();
+  query($("#city").val())
   var city = $("#city").val().trim();
   searches.push(city);
+  
 
   localStorage.setItem("searches", JSON.stringify(searches));
   $("#city").val("");
@@ -24,24 +26,34 @@ $(document).on("click", ".city", function () {
 });
 renderHistory();
 
-localStorage.clear();
+//localStorage.clear();
 
 //api data section
 
-function query(searches) {
-  $("#display").empty();
+function query(city) {
   $.ajax({
-    url:
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      searches +
-      "&units=imperial&appid=c9c478a157f268c22eedc9b26117af86",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c9c478a157f268c22eedc9b26117af86",
     method: "GET",
   }).then(function (response) {
-    console.log(response);
+    //console.log(response);
 
     $(".city").text(response.name);
-    $(".temp").text("Temperature (F) : " + response.main.temp);
+    $(".temp").text("Temperature (F) : " + Math.floor(response.main.temp));
     $(".humidity").text("Humidity: " + response.main.humidity);
     $(".wind").text("Wind Speed: " + response.wind.speed);
   });
+
+   $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=c9c478a157f268c22eedc9b26117af86",
+    method: "GET",
+    }).then(function (response) {
+    console.log(response);
+    var fiveDay = [response.list[4], reponse.list[12], response.list[20], response.list[28], response.list[36]]
+    var fiveText = $("<h2>").text("5 day forecast")
+    $("#display").append(fiveText)
+
+  });
+
 }
+
+
